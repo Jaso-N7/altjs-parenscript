f66effd701c7a45acfa1f4fc6c6599341ac87de5
Jason Robinson <json.robinson@gmail.com>
2022-02-22 01:43:56 -0500

    Borrowed CSS styling from Best MF Website
    Will be converting this base style into CSS-LITE version

--- parenscript.lisp
+++ parenscript.lisp
@@ -186,3 +186,28 @@ margin-bottom: 5px;
 		       ((:div :class "row")
 			((:input :type "submit" :value "Submit"))
 			((:input :type "reset" :value "Reset")))))))))))
+
+#| Style to be used in CSS-Lite from Best MF Website
+<style>
+	@media (prefers-color-scheme: dark){
+		body {color:#fff;background:#000}
+		a:link {color:#cdf}
+		a:hover, a:visited:hover {color:#def}
+		a:visited {color:#dcf}
+	}
+	body{
+		margin:1em auto;
+		max-width:40em;
+		padding:0 .62em;
+		font:1.2em/1.62 sans-serif
+	}
+	h1,h2,h3 {
+		line-height:1.2
+	}
+	@media print{
+		body{
+			max-width:none
+		}
+	}
+</style>
+|#
